603c31ff8681b755498f27a4618599b763fa2268
cristimihaescu <cristimihaescu24@yahoo.ro>
2022-03-17 12:58:09 +0200

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,10 @@
 def is_number(str):
-    pass
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+    
 
 
 def convert_number(str):
@@ -7,23 +12,65 @@ def convert_number(str):
 
 
 def is_valid_operator(operator):
-    pass
+    operatorS = ('+', '-', '*', '/')
+    return operator in operatorS
+    
 
 
 def ask_for_a_number(force_valid_input):
-    pass
+    while True:
+        inp = input("Please provide a number! ")
+        if is_number(inp):
+            return float(inp)
+        else:
+            if not force_valid_input:
+                return None
+            print("This didn't look like a number, try again.")
+    
 
 
 def ask_for_an_operator(force_valid_input):
-    pass
+    while True:
+        operator = input("Please provide an operator (one of +, -, *, /)! ")
+        if is_valid_operator(operator):
+            return operator
+        else:
+            if not force_valid_input:
+                return None
+            print("Unknown operator.")
+    
 
 
 def calc(operator, a, b):
-    pass
+    if not is_valid_operator(operator) or not is_number(a) or not is_number(b):
+        return None
+    result = None
+    if operator == '+':
+        result = a + b
+    elif operator == '-':
+        result = a - b
+    elif operator == '*':
+        result = a * b
+    elif operator == '/':
+        if b != 0:
+            result = a / b
+        else:
+            print("Error: Division by zero")
+            return None
+    return result
 
 
 def simple_calculator():
-    pass
+    while True:
+        a = ask_for_a_number(force_valid_input=False)
+        if a is None:
+            break
+        op = ask_for_an_operator(force_valid_input=True)
+        b = ask_for_a_number(force_valid_input=True)
+        result = calc(op, a, b)
+        if result is not None:
+            print(f"The result is {result}")
+    
 
 
 if __name__ == '__main__':
